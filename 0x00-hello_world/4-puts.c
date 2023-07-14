c43e4a776d084e762d61aa6cdacfd39551728caf
Adolf Kaluba <adolfkaluba@gmail.com>
2023-07-13 20:03:13 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - main entry poit 
-*reuturn : always 0 (success)
+*main -  entry poit 
+*reuturn:  always 0 (success)
 */
 int main(void)
 {
 	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0);               	
 }	
